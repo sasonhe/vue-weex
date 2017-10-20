323c185f82c8af407e3aed62ea85b92903a517be
sasonhe <642139009@qq.com>
2017-10-20 15:12:32 +0800

    add files

---
+++ dist/index.js
@@ -0,0 +1,3 @@
+// { "framework": "Vue"} 
+
+!function(r){function e(o){if(n[o])return n[o].exports;var t=n[o]={i:o,l:!1,exports:{}};return r[o].call(t.exports,t,t.exports,e),t.l=!0,t.exports}var n={};e.m=r,e.c=n,e.i=function(r){return r},e.d=function(r,n,o){e.o(r,n)||Object.defineProperty(r,n,{configurable:!1,enumerable:!0,get:o})},e.n=function(r){var n=r&&r.__esModule?function(){return r.default}:function(){return r};return e.d(n,"a",n),n},e.o=function(r,e){return Object.prototype.hasOwnProperty.call(r,e)},e.p="",e(e.s=3)}({3:function(r,e){throw new Error("Module build failed: Error: ENOENT: no such file or directory, open 'E:\\项目\\awesome-project\\src\\index.vue'")}});
